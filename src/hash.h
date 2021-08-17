9d80dcc5e8c35e6c401ad37404b78dea2a8fb314
John Studnicka <ComputerCraftr@users.noreply.github.com>
2021-08-17 01:40:06 -0700

    Implement scrypt hashing function

--- src/hash.h
+++ src/hash.h
@@ -10,6 +10,7 @@
 #include <attributes.h>
 #include <crypto/common.h>
 #include <crypto/ripemd160.h>
+#include <crypto/scrypt.h>
 #include <crypto/sha1.h>
 #include <crypto/sha256.h>
 //#include <crypto/sha512.h>
@@ -564,6 +565,28 @@ inline uint256 HashNist5(const T1& in1)
 
     uint256 result;
     memcpy(result.data(), hashA, 32);
+    return result;
+}
+
+/* ----------- Scrypt Hash ------------------------------------------------- */
+template<typename T1>
+inline uint256 HashScrypt(const T1& in1)
+{
+    const Span<const unsigned char> &input = MakeUCharSpan(in1);
+
+    uint256 result;
+    scrypt_N_1_1_256(input.data(), input.size(), static_cast<void*>(&result), 1024);
+    return result;
+}
+
+/* ----------- Scrypt² Hash ------------------------------------------------- */
+template<typename T1>
+inline uint256 HashScryptSquared(const T1& in1)
+{
+    const Span<const unsigned char> &input = MakeUCharSpan(in1);
+
+    uint256 result;
+    scrypt_N_1_1_256(input.data(), input.size(), static_cast<void*>(&result), 1048576);
     return result;
 }
 
