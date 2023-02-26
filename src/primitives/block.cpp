e9ca1c0d0f746834551483bd30073d978b28315e
TFinch <nexusnow@protonmail.com>
2023-02-25 23:18:34 -0500

    Revert "Add PoS3.0 consensus and treasury blocks"
    This reverts commit b1af29680e1ddb3ba083db86bf4026cea3cbfd15.

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -23,15 +23,14 @@ uint256 CBlockHeader::GetPoWHash() const
 std::string CBlock::ToString() const
 {
     std::stringstream s;
-    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, type=%i, vtx=%u, vchBlockSig=%u)\n",
+    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, type=%i, vtx=%u)\n",
         GetHash().ToString(),
         nVersion,
         hashPrevBlock.ToString(),
         hashMerkleRoot.ToString(),
         nTime, nBits, nNonce,
         GetAlgoType(nVersion) == -1 ? IsProofOfWork() : GetAlgoType(nVersion),
-        vtx.size(),
-        vchBlockSig.size());
+        vtx.size());
     for (const auto& tx : vtx) {
         s << "  " << tx->ToString() << "\n";
     }
